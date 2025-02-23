5af13dce95488fdaabfd82a17fcceb3c45161e29
Felix Hoffmann <fx.hoffmann93@gmail.com>
2025-02-23 14:35:02 +0100

    feat: updated config

--- src/config.js
+++ src/config.js
@@ -2,33 +2,35 @@
 
 // Language to use for the calendar events
 // Change this to the desired language ('en', 'de', 'es', 'fr')
-// var language = 'de';
+// const language = 'de';
 
 // The ID of the Google Calendar where birthdays will be added.
 // You can find this ID in your Google Calendar settings.
-var calendarId = '99406ed07130a00e82235b91df15a0fe67a28b8fd7bbaf08f81fff2fd2b77a9a@group.calendar.google.com';
+const calendarId = '99406ed07130a00e82235b91df15a0fe67a28b8fd7bbaf08f81fff2fd2b77a9a@group.calendar.google.com';
 
 // Set this to true if you want to sync birthdays only for contacts with a specific label(s).
-var useLabel = false;
+const useLabel = false;
 
 // The name(s) of the label(s) to be used if useLabel is set to true.
 // Only contacts with one of these labels will be fetched.
-// var labelFilter = ["❤️ Gemeinsam", "👮 Hannover"];
+// const labelFilter = ["❤️ Gemeinsam", "👮 Hannover"];
 
 // The type of reminder to be added to the birthday events.
 // Possible values: 'none', 'email', 'popup'.
-var reminderMethod = 'popup';
+const reminderMethod = 'popup';
 
 // The number of minutes before the event when the reminder should popup or send an email.
 // For example, 60 * 12 means the reminder will be sent 12 hours before the event.
-var reminderInMinutes = 60 * 12;
+const reminderInMinutes = 60 * 12;
 
 // Set this to true if you want to create individual birthday events for each contact.
-var createIndividualBirthdayEvents = true;
+const createIndividualBirthdayEvents = true;
 // Set this to true if you want to create monthly birthday summary events.
 // = one event on the 1st of each month containg all birthdays this month. 
-var createBirthdaySummaryEvents = true;
+const createBirthdaySummaryEvents = true;
 
 // The amount of months ahead for which birthday events and summaries will be created/updated.
 // By default, it creates events for the next year (=12 months).
-var monthsAhead = 12;
+const monthsAhead = 12;
+
+const enableContactPhotos = false;
